e99d707728742212248eb965d181b6ecac82d368
Victor Luo <vluo@mit.edu>
2021-11-16 18:37:40 -0500

    feat: have a map

---
+++ src/components/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <GmapMap
+    :center="{lat:10, lng:10}"
+    :zoom="7"
+    map-type-id="terrain"
+    class="map"
+  >
+    <GmapMarker
+      :key="index"
+      v-for="(m, index) in markers"
+      :position="m.position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=m.position"
+    />
+  </GmapMap>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  data: function () {
+    return {};
+  },
+}
+</script>
+
+<style scoped>
+.map {
+  width: 500px;
+  height: 300px;
+}
+</style>
