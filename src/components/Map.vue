44af42c00fe471c49b4708336572f63ccc7bb30a
Victor Luo <vluo@mit.edu>
2021-11-17 19:09:42 -0500

    minor: refactor in preparation to make things work

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,27 +1,44 @@
 <template>
-  <GmapMap
-    :center="{lat:10, lng:10}"
-    :zoom="7"
-    map-type-id="terrain"
-    class="map"
-  >
-    <GmapMarker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      :draggable="true"
-      @click="center=m.position"
-    />
-  </GmapMap>
+  <div id="map-container">
+    <GmapMap
+      :center="center"
+      :zoom="7"
+      class="map"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="onMarkerClick(m.position)"
+      />
+    </GmapMap>
+    <p>Double click on the map to create a new blockage.</p>
+    <CreateBlockage />
+  </div>
 </template>
 
 <script>
+import CreateBlockage from '@/components/CreateBlockage.vue';
+
 export default {
   name: 'Map',
+  components: { CreateBlockage },
   data: function () {
-    return {};
+    return {
+      center: {lat:10, lng:10},
+      markers: [
+        {position:{lat:10, lng:10}}
+      ],
+    };
   },
+  methods: {
+    onMarkerClick: function(pos) {
+      console.log(this.center);
+      this.center = pos;
+    }
+  }
 }
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,23 @@
 <template>
   <main>
     <Map/>
-    <div class="home">
-      <!-- Hello world -->
-      Double click on the map to create a new blockage.
-    </div>
-    <CreateBlockage/>
     <Blockage/>
   </main>
 </template>
 
 <script>
-import Map from '../components/Map.vue';
-
-import CreateBlockage from '@/components/CreateBlockage.vue';
 import Blockage from '@/components/Blockage.vue';
+import Map from '../components/Map.vue';
 // import App from '../App.vue';
 
 export default {
   name: 'Home',
-  components: { CreateBlockage, Blockage, Map}
+  components: { Blockage, Map }
 }
 </script>
 
 <style scoped>
-.home {
+main {
   display: flex;
   flex-direction: column;
   align-items: center;
