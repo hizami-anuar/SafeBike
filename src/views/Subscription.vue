f7c3b83d818078acd578a3e2eeab2cbaf6b562ea
hizami-anuar <59573333+hizami-anuar@users.noreply.github.com>
2021-11-24 13:05:09 -0500

    cleanup useless code

--- src/views/Subscription.vue
+++ src/views/Subscription.vue
@@ -39,7 +39,7 @@
 <script>
 import axios from 'axios';
 import { eventBus } from "@/main";
-import Map from '@/components/Map.vue';
+import Map from '@/components/Map';
 
 export default {
   name: 'Subscription',
